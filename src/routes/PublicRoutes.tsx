cc6c70cee4f6b97f046f3eead3da76265b417ff6
NeelSolguruz <neelshah0503@gmail.com>
2025-02-16 16:11:04 +0530

    fix: change the navigation routes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 const PublicRoutes = () => {
   const { userInfo } = useAppStore()
   const isAutheticated = !!userInfo
-  return isAutheticated ? <Navigate to="/chat" /> : <Outlet />
+  return isAutheticated ? <Navigate to="/" /> : <Outlet />
 }
 
 export default PublicRoutes
